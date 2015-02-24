17021c78be64aea04cddd49346ac8ab26a48bd60
Alan Barr <alanbarr2002@hotmail.com>
2015-02-24 21:13:03 +0000

    Adding git.

--- setup.sh
+++ setup.sh
@@ -6,18 +6,19 @@ set -e
 
 git submodule update --init --recursive
 
-# Setup vim
+########## Setup vim ########## 
 ln -s ${DOT_DIR}/vim/.vimrc   ${HOME_DIR}/.vimrc
 ln -s ${DOT_DIR}/vim/.vim     ${HOME_DIR}/.vim
 
 ln -s ${DOT_DIR}/vim/.vim/colors/wombat256/colors/wombat256mod.vim  ${HOME_DIR}/.vim/colors/wombat256mod.vim
 ln -s ${DOT_DIR}/vim/.vim/colors/wombat/colors/wombat.vim           ${HOME_DIR}/.vim/colors/wombat.vim
 
-
-# Setup Fonts
+########## Setup Fonts ########## 
 ln -s ${DOT_DIR}/fonts ${HOME_DIR}/.fonts
 
-
+########## Setup Git ########## 
+ln -s ${DOT_DIR}/git/.gitconfig ${HOME_DIR}/.gitconfig
+ln -s ${DOT_DIR}/git/.gitignore ${HOME_DIR}/.gitignore
 
 
 
