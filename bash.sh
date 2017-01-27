c71445cbe1474ea81e0c4735b8410d3a1d1d17b0
Alan Barr <a.barr@outlook.com>
2017-01-27 14:07:49 +0000

    [bash] Tweak history settings

--- bash.sh
+++ bash.sh
@@ -1,6 +1,22 @@
+################################################################################
+# History
+################################################################################
 HISTCONTROL=
 HISTSIZE=2000
-HISTFILESIZE=20000
+HISTFILESIZE=200000
 HISTTIMEFORMAT="(%F %T) "
+# Append history to file rather than overwrite
+shopt -s histappend
+# Save and reload history after each command
+# export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+# Save history after each command
+export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
+################################################################################
+# Misc
+################################################################################
+
+# Use vi commands on the command line rather than Emacs. 
+# No more Ctrl-A etc
+# Instead ESC then vi command
 set -o vi
